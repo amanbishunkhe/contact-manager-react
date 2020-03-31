7fea18a58735774cfb9211de0c509db63db1490c
amanbishunkhe <aman.rigorousweb@gmail.com>
2020-03-31 13:30:14 +0545

    Updates

---
+++ precache-manifest.73d4366b25dc1b4bd8720bb6a5c25bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906b577935683a3504513f97fdb6adf1",
+    "url": "/contact-manager-react/index.html"
+  },
+  {
+    "revision": "ce22152dbd0facc2060b",
+    "url": "/contact-manager-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "aabe038ab262e3b4a751",
+    "url": "/contact-manager-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ce22152dbd0facc2060b",
+    "url": "/contact-manager-react/static/js/2.1453c146.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/contact-manager-react/static/js/2.1453c146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aabe038ab262e3b4a751",
+    "url": "/contact-manager-react/static/js/main.df7decb3.chunk.js"
+  },
+  {
+    "revision": "9393536f967c591da926",
+    "url": "/contact-manager-react/static/js/runtime-main.b7adc0ed.js"
+  }
+]);
